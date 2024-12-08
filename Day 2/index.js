453c37e77d8e1be0a2b1bd63a8f2b7a3d3b9d05f
Luis Moreno <luis.moreno@exatec.tec.mx>
2024-12-07 18:02:30 -0600

    Add week days constant

--- Day 2/index.js
+++ Day 2/index.js
@@ -1,8 +1,13 @@
 const calendarContainer = document.getElementById('calendar');
 
-for (let i = 1; i <= 24; i++) {
+const weekDays = [
+  "Monday", "Tuesday", "Wednesday",
+  "Thursday", "Friday", "Saturday", "Sunday"
+]
+
+for (let i = 1; i <= 31; i++) {
   let box = document.createElement('li');
-  box.classList.add('calendar-box');
+  box.classList.add('calendar-circle');
   
   let number = document.createElement('p');
   number.innerHTML = i;
