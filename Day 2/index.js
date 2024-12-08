877b677a0f3a8dcf2285eac2058a726c18082435
Luis Moreno <luis.moreno@exatec.tec.mx>
2024-12-07 20:39:53 -0600

    Add weekday names to calendar

--- Day 2/index.js
+++ Day 2/index.js
@@ -1,11 +1,24 @@
 const calendarContainer = document.getElementById('calendar');
 
 const weekDays = [
-  "Monday", "Tuesday", "Wednesday",
-  "Thursday", "Friday", "Saturday", "Sunday"
+  "Sunday", "Monday", "Tuesday",
+  "Wednesday", "Thursday", "Friday", "Saturday"
 ]
 
-for (let i = -7; i <= 31; i++) {
+for (let weekDay of weekDays) {
+  let weekDayBox = document.createElement('li')
+  weekDayBox.classList.add("calendar-weekday-box")
+
+  let weekDayName = document.createElement('p')
+  weekDayName.innerHTML = weekDay.slice(0,3).toUpperCase()
+  weekDayName.classList.add('weekday-name')
+
+  weekDayBox.appendChild(weekDayName)
+
+  calendarContainer.appendChild(weekDayBox)
+}
+
+for (let i = 1; i <= 31; i++) {
   
   let circle = document.createElement('li');
   circle.classList.add('calendar-circle');
