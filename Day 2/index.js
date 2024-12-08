2494a8565675b6d19c7162301a354fa29a9d76a2
Luis Moreno <luis.moreno@exatec.tec.mx>
2024-12-07 21:06:20 -0600

    Wrap weekday names and calendar circles in their own functions

--- Day 2/index.js
+++ Day 2/index.js
@@ -1,37 +1,41 @@
 const calendarContainer = document.getElementById('calendar');
 
-const weekDays = [
-  "Sunday", "Monday", "Tuesday",
-  "Wednesday", "Thursday", "Friday", "Saturday"
-]
+const weekDays = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 
-for (let weekDay of weekDays) {
-  let weekDayBox = document.createElement('li')
-  weekDayBox.classList.add("calendar-weekday-box")
+addWeekDays();
+addCalendarDays();
 
-  let weekDayName = document.createElement('p')
-  weekDayName.innerHTML = weekDay.slice(0,3).toUpperCase()
-  weekDayName.classList.add('weekday-name')
-
-  weekDayBox.appendChild(weekDayName)
-
-  calendarContainer.appendChild(weekDayBox)
+function addWeekDays() {
+  for (let weekDay of weekDays) {
+    let weekDayBox = document.createElement('li')
+    weekDayBox.classList.add("calendar-weekday-box")
+  
+    let weekDayName = document.createElement('p')
+    weekDayName.innerHTML = weekDay.slice(0,3).toUpperCase()
+    weekDayName.classList.add('weekday-name')
+  
+    weekDayBox.appendChild(weekDayName)
+  
+    calendarContainer.appendChild(weekDayBox)
+  }
 }
 
-for (let i = 1; i <= 31; i++) {
+function addCalendarDays() {
+  for (let i = 1; i <= 31; i++) {
   
-  let circle = document.createElement('li');
-  circle.classList.add('calendar-circle');
-  
-  let number = document.createElement('p');
-  number.innerHTML = i;
-  number.classList.add("calendar-day")
-  
-  const icon = document.createElement('i');
-  icon.classList.add('fas', 'fa-gift');
-  
-  circle.appendChild(number);
-  circle.appendChild(icon);
-  
-  calendarContainer.appendChild(circle);
+    let circle = document.createElement('li');
+    circle.classList.add('calendar-circle');
+    
+    let number = document.createElement('p');
+    number.innerHTML = i;
+    number.classList.add("calendar-day")
+    
+    const icon = document.createElement('i');
+    icon.classList.add('fas', 'fa-gift');
+    
+    circle.appendChild(number);
+    circle.appendChild(icon);
+    
+    calendarContainer.appendChild(circle);
+  }
 }
